438ee585b1b4698d62668b1a1ea4002714ea86af
miamzimela <mia.mzimela@umuzi.org>
2026-08-05 21:36:11 +0200

    Add semantic structure and break App into components
    - Replaced root div with React fragment in App.js
    - Created Header, Nav, Main, and Footer components
    - Added Little Lemon logo to Header component
    - Added navigation links to Nav component
    - Added meta tags (description, og:title, og:description, og:image) to index.html
    - Added logo and lemon dessert image assets

---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+function Footer() {
+  return (
+    <footer>
+      <div>Little Lemon</div>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/reservations">Reservations</a></li>
+        </ul>
+      </nav>
+      <div>
+        <p>123 Chicago Ave, Chicago, IL</p>
+        <p>Open daily: 5pm - 11pm</p>
+      </div>
+      <div>
+        <a href="#">Facebook</a>
+        <a href="#">Instagram</a>
+        <a href="#">Twitter</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import logo from './images/logo.png';
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon Logo" />
+    </header>
+  );
+}
+
+export default Header;
